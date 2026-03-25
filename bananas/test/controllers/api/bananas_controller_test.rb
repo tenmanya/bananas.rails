85cd611dfaa3fd9eb07928ba078b680528825811
Jahn Saito <jahn.saito@gmail.com>
2026-03-25 10:11:59 +0100

    Update bananas_controller_test.rb

--- bananas/test/controllers/api/bananas_controller_test.rb
+++ bananas/test/controllers/api/bananas_controller_test.rb
@@ -4,6 +4,6 @@ class Api::BananasControllerTest < ActionDispatch::IntegrationTest
   test "GET /api/bananas returns yellow: true" do
     get "/api/bananas"
     assert_response :ok
-    assert_equal({ "yellow" => true, bunch => 7 }, response.parsed_body)
+    assert_equal({ "yellow" => true, "bunch" => 7 }, response.parsed_body)
   end
 end
